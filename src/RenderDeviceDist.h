5dc6d871cc10f64229b91aa5da8714a1858b799a
Taylor Beebe <tdb1@williams.edu>
2019-04-29 18:39:11 -0400

    moved definitions for texturedist and renderdevicedist to .h file. Added framebufferdist.h

--- src/RenderDeviceDist.h
+++ src/RenderDeviceDist.h
@@ -3,6 +3,14 @@
 
 class RenderDeviceDist : public RenderDevice {
 
-	void pushState(const shared_ptr<Framebuffer>& fb);
+	void pushState(const shared_ptr<Framebuffer>& fb) {
+		
+		RenderDevice::pushState();
 
+		if (fb) {
+			setFramebuffer(fb);
+			setClip2D(Rect2D::xyxy(width() / 2, height() / 2, width(), height()));
+			setViewport(fb->rect2DBounds());
+		}
+	}
 };
