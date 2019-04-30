b0b1264102a935ae1794a003d81d27992af39048
Taylor <tdb1@williams.edu>
2019-04-30 19:02:57 -0400

    framebufferdist.h working

--- src/FramebufferDist.h
+++ src/FramebufferDist.h
@@ -6,12 +6,89 @@ namespace G3D {
 
 	class FramebufferDist : public Framebuffer {
 
-	public:
+	protected:
 
+		FramebufferDist(const String& name, GLuint framebufferID) : Framebuffer(name, framebufferID) { }
+
+	public:
+		
+		
+		static shared_ptr<FramebufferDist> create(const String& _name) {
+			GLuint _framebufferID;
+
+			// Generate Framebuffer
+			glGenFramebuffers(1, &_framebufferID);
+
+			return shared_ptr<FramebufferDist>(new FramebufferDist(_name, _framebufferID));
+		}
+		
+		/*
 		const shared_ptr<TextureDist>& texture(const uint8 x) {
 			return texture(AttachmentPoint(x + COLOR0));
 		}
+		*/
 
+		static shared_ptr<FramebufferDist> create(const shared_ptr<Texture>& t0, const shared_ptr<Texture>& t1 = nullptr, const shared_ptr<Texture>& t2 = nullptr, const shared_ptr<Texture>& t3 = nullptr) {
+			shared_ptr<FramebufferDist> f = create(t0->name() + " framebuffer");
 
+			if (t0) {
+				if (t0->format()->depthBits > 0) {
+					if (t0->format()->stencilBits > 0) {
+						f->set(DEPTH_AND_STENCIL, t0);
+					}
+					else {
+						f->set(DEPTH, t0);
+					}
+				}
+				else {
+					f->set(COLOR0, t0);
+				}
+			}
+
+			if (t1) {
+				if (t1->format()->depthBits > 0) {
+					if (t1->format()->stencilBits > 0) {
+						f->set(DEPTH_AND_STENCIL, t1);
+					}
+					else {
+						f->set(DEPTH, t1);
+					}
+				}
+				else {
+					f->set(COLOR1, t1);
+				}
+			}
+
+			if (t2) {
+				if (t2->format()->depthBits > 0) {
+					if (t2->format()->stencilBits > 0) {
+						f->set(DEPTH_AND_STENCIL, t2);
+					}
+					else {
+						f->set(DEPTH, t2);
+					}
+				}
+				else {
+					f->set(COLOR2, t2);
+				}
+			}
+
+			if (t3) {
+				if (t3->format()->depthBits > 0) {
+					if (t3->format()->stencilBits > 0) {
+						f->set(DEPTH_AND_STENCIL, t3);
+					}
+					else {
+						f->set(DEPTH, t3);
+					}
+				}
+				else {
+					f->set(COLOR3, t3);
+				}
+			}
+
+			return f;
+		}
+		
 	};
 }
